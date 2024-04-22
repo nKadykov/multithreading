8d80c2fbb04f6ac458892e486ca941b11e9306a1
nKadykov <nkadykov@gmail.com>
2024-04-22 19:47:13 +0300

    init

--- scoped_lock.cpp
+++ scoped_lock.cpp
@@ -7,12 +7,12 @@ int main() {
 
     auto f1 = [&m1, &m2]() {
         std::scoped_lock lg(m1, m2);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     };
 
     auto f2 = [&m1, &m2]() {
         std::scoped_lock lg(m1, m2);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     };
 
     std::thread thread1([&f1, &f2]() {
