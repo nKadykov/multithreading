f29963a75fe133d496168aff5d98bf63f92fe353
nKadykov <nkadykov@gmail.com>
2024-04-29 16:15:49 +0300

    init

---
+++ timed_mutex_2.cpp
@@ -0,0 +1,18 @@
+#include <thread>
+#include <iostream>
+#include <chrono>
+#include <mutex>
+
+std::timed_mutex test_mutex;
+
+void f() {
+    auto now = std::chrono::steady_clock::now();
+    test_mutex.try_lock_until(now + std::chrono::seconds(10));
+    std::cout << "Hello world\n";
+}
+
+int main() {
+    std::lock_guard<std::timed_mutex> l(test_mutex);
+    std::thread t(f);
+    t.join();
+}
