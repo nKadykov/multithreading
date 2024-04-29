a803bcfc7092bfbc898020f774fc5174c20297f5
nKadykov <nkadykov@gmail.com>
2024-04-29 16:21:33 +0300

    init

---
+++ lock_guard_2.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+int g_i = 0;
+std::mutex g_i_mutex;
+
+void safe_increment() {
+    const std::lock_guard<std::mutex> lock(g_i_mutex);
+    ++g_i;
+    std::cout << "g_i: " << g_i << "; in thread " << std::this_thread::get_id() << '\n';
+}
+
+int main() {
+    std::cout << "g_i: " << g_i << "; in main()\n";
+    std::thread t1(safe_increment);
+    std::thread t2(safe_increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "g_i: " << g_i << "; in main()\n";
+}
